5b37648a02569f093913f97bd40da01b9c49ea8a
tedoaba <tedoaba@gmail.com>
2024-10-10 22:25:54 +0300

    dbt

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,7 +2,7 @@ import pandas as pd
 import logging
 
 # Set up logging
-logging.basicConfig(filename='logs/data_cleaning.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(filename='../logs/data_cleaning.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 def clean_data(file_path):
     try:
@@ -16,7 +16,7 @@ def clean_data(file_path):
 
         # Handle missing values
         df_cleaned.dropna(subset=['text'], inplace=True)
-        df_cleaned['sender_id'].fillna('Unknown', inplace=True)
+        df_cleaned.fillna({'sender_id':'Unknown'}, inplace=True)
 
         # Standardize date format
         df_cleaned['date'] = pd.to_datetime(df_cleaned['date'], errors='coerce')
@@ -37,5 +37,5 @@ def clean_data(file_path):
         raise
 
 if __name__ == '__main__':
-    file_path = 'telegram_medical_businesses_data.csv'  # Update this path as needed
+    file_path = '../data/telegram_medical_businesses_data.csv'  # Update this path as needed
     clean_data(file_path)
