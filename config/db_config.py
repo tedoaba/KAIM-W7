ba0ac81628e5e9a6fabe75cee905ac26a487959d
tedoaba <tedoaba@gmail.com>
2024-10-11 00:14:34 +0300

    dbt full run

--- config/db_config.py
+++
@@ -1,7 +0,0 @@
-db_params = {
-    'dbname': 'my_database',
-    'user': 'my_user',
-    'password': 'my_password',
-    'host': 'localhost',
-    'port': '5432'
-}
--- dbt_project/models/telegram_data.sql
+++
@@ -1,9 +0,0 @@
--- models/telegram_data.sql
-
-SELECT 
-    message_id,
-    date,
-    text,
-    sender_id
-FROM 
-    telegram_medical_businesses;
